310cf9e4272619280e4d515fbc64b467608acd2f
Devika2012-svg <preetraju@gmail.com>
2026-05-23 09:31:43 +0400

    Add files via upload

---
+++ Birthday.py
@@ -0,0 +1,19 @@
+name1 = "Devika"
+bday1 = input("Enter birthday 1: ")
+print(name1, "-", bday1)
+
+name2 = "Anndrea"
+bday2 = input("Enter birthday 2: ")
+print(name2, "-", bday2)
+
+name3 = "Jaza"
+bday3 = input("Enter birthday 3: ")
+print(name3, "-", bday3)
+
+name4 = "Anna"
+bday4 = input("Enter birthday 4: ")
+print(name4, "-", bday4)
+
+name5 = "Misna"
+bday5 = input("Enter birthday 5: ")
+print(name5, "-", bday5)
