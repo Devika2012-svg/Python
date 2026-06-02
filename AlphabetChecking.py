b7607396481f6fe27ac807216f6948be8cd20346
Devika2012-svg <preetraju@gmail.com>
2026-06-02 21:27:29 +0400

    Update AlphabetChecking.py

--- AlphabetChecking.py
+++ AlphabetChecking.py
@@ -2,13 +2,13 @@ character = input("Enter a character: ")
 
 print(">>>>>>>Recognising the character<<<<<<<<")
 if (character >= 'A' and character <= 'Z') or (character >= 'a' and character <= 'z'):
-    print("It is an alphabet.")
+    print("It is an alphabet. 🅰")
 
 elif character >= '0' and character <= '9':
-    print("It is a number.")
+    print("It is a number. 1️⃣")
 
-elif character == '*' or character == '@' or character == '!' or character == '#':
-    print("It is a special character.")
+elif character == '*' or character == '@' or character == '!' or character == '#' or character == '?' or character == '&' or character == '*'   :
+    print("It is a special character. ❗")
 
 else:
     print("Character not recognized.")
